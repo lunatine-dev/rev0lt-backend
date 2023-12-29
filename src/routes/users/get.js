9ed2d3c91b13f72124e8bd82e48eb9ea12754c9c
Morgan Evans <morgan@morganevans.dev>
2023-12-29 22:50:43 +0000

    change sort to total points

--- src/routes/users/get.js
+++ src/routes/users/get.js
@@ -3,7 +3,7 @@ const User = require("../../models/User");
 const routes = (fastify, opts, done) => {
     fastify.get("/", async (req, res) => {
         let users = await User.find({}).sort({
-            points: -1,
+            total_points: -1,
         });
 
         return users;
