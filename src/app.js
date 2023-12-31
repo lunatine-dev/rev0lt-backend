baa1af13e3869058329446b750c484fcb03a86a3
Morgan Evans <morgan@morganevans.dev>
2023-12-31 20:12:02 +0000

    Fixed non same origin bug

--- src/app.js
+++ src/app.js
@@ -27,9 +27,6 @@ const path = require("path"),
             // allow from env.FRONTEND_URL
             origin: process.env.FRONTEND_URL,
         });
-        server.register(require("@fastify/helmet"), {
-            crossOriginResourcePolicy: false,
-        });
         server.register(require("@fastify/static"), {
             root: path.join(__dirname, "public"),
             prefix: "/public/", // optional: default '/'
